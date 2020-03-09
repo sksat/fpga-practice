0d7ffe29b86eb8ecb4241e3a31d73a8623fa7929
sksat <sksat@sksat.net>
2020-03-09 16:53:16 +0900

    [update] mux

--- multiplexer/src/mux.v
+++ multiplexer/src/mux.v
@@ -5,6 +5,15 @@ module mux(
 	output wire out
 );
 
-	assign out = (select ? A : B);
+	wire nsel;
+	wire c1;
+	wire c2;
+
+	mynot mux_mynot(select, nsel);
+	myand mux_myand1(nsel, A, c1);
+	myand mux_myand2(select, B, c2);
+	myor mux_myor(c1, c2, out);
+
+//	assign out = (select ? A : B);
 
 endmodule
--- multiplexer/src/top.v
+++ multiplexer/src/top.v
@@ -4,10 +4,10 @@ module top(
 	output wire [2:0]RGB_LED	// 0の時点灯
 );
 
-//mux mux0(1, 0, RST_N, RGB_LED[0]);
-//mux mux1(0, 1, RST_N, RGB_LED[1]);
+mux mux0(1, 0, RST_N, RGB_LED[0]);
+mux mux1(0, 1, RST_N, RGB_LED[1]);
 
-myxor(0, RST_N, RGB_LED[0]);
+//myxor(0, RST_N, RGB_LED[0]);
 
 //myor(0, RST_N, RGB_LED[0]);
 
@@ -16,7 +16,7 @@ myxor(0, RST_N, RGB_LED[0]);
 //mynot(RST_N, RGB_LED[0]);
 
 //assign RGB_LED[0] = 1;
-assign RGB_LED[1] = 1;
+//assign RGB_LED[1] = 1;
 assign RGB_LED[2] = 1;
 
 endmodule
